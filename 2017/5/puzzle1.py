da0f1f35b40c59874f7a52a718bbacb0ec955180
Henrique Ferrolho <henriqueferrolho@gmail.com>
2017-12-05 15:36:19 +0000

    Adds day 5 puzzle 1 and 2

---
+++ 2017/5/puzzle1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def main():
+    instructions = [int(jump.strip()) for jump in sys.stdin]
+    current = 0
+    counter = 0
+
+    while 0 <= current and current < len(instructions):
+        currentAux = current
+        current += instructions[current]
+        instructions[currentAux] += 1
+        counter += 1
+
+    print("  Steps taken until exit:", counter)
+
+
+if __name__ == '__main__':
+    main()
