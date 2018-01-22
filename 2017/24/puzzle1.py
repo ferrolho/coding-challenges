3555f5a8da9442f239ca392f3417dce870a1d20e
Henrique Ferrolho <henriqueferrolho@gmail.com>
2018-01-22 23:52:35 +0000

    Adds day 24 puzzle 1 and 2

---
+++ 2017/24/puzzle1.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+import copy
+import numpy as np
+import sys
+
+flatten = lambda l: [item for sublist in l for item in sublist]
+strength = lambda bridge: sum([sum([int(port) for port in component.split('/')]) for component in bridge])
+
+max_strength = 0
+
+
+class Bridge():
+    def __init__(self, head):
+        self.components = [head]
+        self.tailPins = sum([int(port) for port in head.split('/')])
+
+
+def dfs(bridge, components_available):
+    global max_strength
+
+    if not components_available or \
+       bridge.tailPins not in np.unique(flatten([[int(pin) for pin in c.split('/')] for c in components_available])):
+        if strength(bridge.components) > max_strength:
+            max_strength = strength(bridge.components)
+            print(max_strength, bridge.components)
+    else:
+        for component in components_available:
+            if bridge.tailPins in [int(port) for port in component.split('/')]:
+                extension = copy.deepcopy(bridge)
+                extension.components.append(component)
+                extension.tailPins = sum([int(port) for port in component.split('/')]) - extension.tailPins
+                dfs(extension, [c for c in components_available if c != component])
+
+
+def main():
+    components = [component.strip() for component in sys.stdin.readlines()]
+    zeroPinPorts = [component for component in components if 0 in [int(port) for port in component.split('/')]]
+
+    for zeroPinPort in zeroPinPorts:
+        dfs(Bridge(zeroPinPort), [c for c in components if c != zeroPinPort])
+
+    print(max_strength)
+
+
+if __name__ == '__main__':
+    main()
