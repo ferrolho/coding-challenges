b98c9013ef09d40466abbe74488d23b704a41893
Henrique Ferrolho <henrique.ferrolho@gmail.com>
2019-12-03 23:40:07 +0000

    Finishes day 1

--- 2019/01/puzzle.jl
+++ 2019/01/puzzle.jl
@@ -1,9 +1,19 @@
 using DelimitedFiles
 
 function day1()
-    numbers = readdlm("./01/input.txt", Int)
+    numbers = vec(readdlm("./01/input.txt", Int))
+
     map!(x -> floor(x / 3) - 2, numbers, numbers)
-    sum(numbers)
+    result_1 = sum(numbers)
+
+    result_2 = result_1
+    while !isempty(numbers)
+        map!(x -> floor(x / 3) - 2, numbers, numbers)
+        filter!(x -> x ≥ 0, numbers)
+        result_2 += sum(numbers)
+    end
+
+    result_1, result_2
 end
 
 export day1
