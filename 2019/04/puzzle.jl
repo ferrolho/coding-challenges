79c4c1825a91fa6518f597350f7884197684a66e
Henrique Ferrolho <henrique.ferrolho@gmail.com>
2019-12-04 10:29:47 +0000

    Adds day 4

---
+++ 2019/04/puzzle.jl
@@ -0,0 +1,37 @@
+using DelimitedFiles
+
+function day4()
+    input = vec(readdlm("./04/input.txt", '-', Int))
+    range = input[1]:input[2]
+
+    not_decreasing(s) = join(sort(collect(s))) == s
+
+    function rule_1(s)
+        # Scan string
+        for i = 1:length(s)-1
+            s[i] == s[i+1] && return true
+        end
+        return false
+    end
+
+    function rule_2(s)
+        # Corner cases
+        s[1] == s[2] != s[3] && return true
+        s[end-2] != s[end-1] == s[end] && return true
+        # Check rest of the string
+        for i = 2:length(s)-2
+            s[i-1] != s[i] == s[i+1] != s[i+2] && return true
+        end
+        return false
+    end
+
+    meets_criteria_1(s) = not_decreasing(s) && rule_1(s)
+    meets_criteria_2(s) = not_decreasing(s) && rule_2(s)
+
+    result_1 = count(x -> meets_criteria_1(string(x)), range)
+    result_2 = count(x -> meets_criteria_2(string(x)), range)
+
+    result_1, result_2
+end
+
+export day4
--- 2019/src/Advent2019.jl
+++ 2019/src/Advent2019.jl
@@ -8,6 +8,7 @@ greet() = println("Hello, Advent of Code 2019!")
 include("../01/puzzle.jl")
 include("../02/puzzle.jl")
 include("../03/puzzle.jl")
+include("../04/puzzle.jl")
 
 export greet
 
