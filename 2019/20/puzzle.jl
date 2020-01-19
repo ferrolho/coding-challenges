3d5e77d5eb07e6e5a47f7b1d170f1345effaa6b2
Henrique Ferrolho <henrique.ferrolho@gmail.com>
2020-01-19 18:51:46 +0000

    Adds day 20 (part 2)

--- 2019/20/puzzle.jl
+++ 2019/20/puzzle.jl
@@ -65,9 +65,60 @@ function day20()
         return -1
     end
 
-    result₁ = shortest_path(input)
+    function shortest_path_part2(grid::Array{Char})
+        start = labels["AA"] |> first
+        final = labels["ZZ"] |> first
+        # println(start)
+        # println(final)
 
-    # result₁, result₂
+        q = Queue{Tuple{CartesianIndex,Int}}()
+        visited = Set{Tuple{CartesianIndex,Int}}()
+
+        enqueue!(q, (start, 0))
+        push!(visited, (start, 0))
+
+        n_steps = 0
+
+        while !isempty(q)
+            q_size = length(q)
+            for _ = 1:q_size
+                cur, lvl = dequeue!(q)
+                (cur == final && lvl == 0) && return n_steps
+                for dir ∈ [CartesianIndex(1, 0), CartesianIndex(0, 1),
+                           CartesianIndex(-1, 0), CartesianIndex(0, -1)]
+                    next = cur + dir
+                    next_lvl = lvl
+                    grid[next] == '#' && continue
+                    if grid[next] |> isuppercase
+                        coords = [v for (k, v) in labels if cur ∈ v] |> first
+                        portal_exit = setdiff(coords, [cur])
+                        if isempty(portal_exit)
+                            continue  # Bumped against AA or ZZ
+                        else
+                            next = first(portal_exit)  # coordinate after warp
+                            if any(==(3), cur.I) || any(==(2), size(grid) .- cur.I)
+                                next_lvl > 0 ? (next_lvl -= 1) : continue  # Going out...
+                            else
+                                next_lvl += 1  # Going in...
+                            end
+                        end
+                    end
+                    if (next, next_lvl) ∉ visited
+                        enqueue!(q, (next, next_lvl))
+                        push!(visited, (next, next_lvl))
+                    end
+                end
+            end
+            n_steps += 1
+            # print("\rSteps: $(n_steps)    Queue: $(length(q))")
+        end
+        return -1
+    end
+
+    result₁ = shortest_path(input)
+    result₂ = shortest_path_part2(input)
+
+    result₁, result₂
 end
 
 export day20
