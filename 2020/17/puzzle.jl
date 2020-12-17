b8aad8a5aba7519a2ff0470833a99b57e733b463
Henrique Ferrolho <henriqueferrolho@gmail.com>
2020-12-17 15:45:01 +0000

    Improves day 17

--- 2020/17/puzzle.jl
+++ 2020/17/puzzle.jl
@@ -2,22 +2,14 @@ function day17()
     input = joinpath(@__DIR__, "input.txt")
     state₀ = mapreduce(collect, hcat, eachline(input))
 
-    neighbours3D = [(i, j, k)
-                    for i = -1:1, j = -1:1, k = -1:1
-                    if !all(iszero, (i, j, k))] .|> CartesianIndex
-
-    neighbours4D = [(i, j, k, l)
-                    for i = -1:1, j = -1:1, k = -1:1, l = -1:1
-                    if !all(iszero, (i, j, k, l))] .|> CartesianIndex
-
     isactive = ==('#')
 
     select(A, I, neighbours) = map(neighbours) do step
         checkbounds(Bool, A, I + step) ? A[I + step] : return nothing
     end
 
-    function solve(state₀, neighbours; cycles=6)
-        dims = length(first(neighbours))  # Infer dimensionality from `neighbours`
+    function solve(state₀; cycles=6, dims=3)
+        neighbours = filter(!iszero, CartesianIndices(ntuple(_ -> -1:1, dims)))
 
         state₀ = reshape(state₀, size(state₀)..., fill(1, dims - 2)...)
 
@@ -28,11 +20,11 @@ function day17()
 
         state⁻ = similar(state⁺)  # We need to track the previous state
 
-        for i = 1:cycles
+        for _ = 1:cycles
 
             copyto!(state⁻, state⁺)
 
-            for i ∈ CartesianIndices(state⁺)
+            @inbounds for i ∈ CartesianIndices(state⁺)
 
                 if state⁻[i] |> isactive
                     active = count(isactive, select(state⁻, i, neighbours))
@@ -49,11 +41,8 @@ function day17()
         count(isactive, state⁺)
     end
 
-    result₁ = solve(state₀, neighbours3D)
-    result₂ = solve(state₀, neighbours4D)
-
-    @assert result₁ == 265
-    @assert result₂ == 1936
+    result₁ = solve(state₀, dims=3)
+    result₂ = solve(state₀, dims=4)
 
     result₁, result₂
 end
