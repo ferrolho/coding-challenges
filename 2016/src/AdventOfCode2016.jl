db5f4f7da93ef9c8aaf1d0d0824efab1d5539bb2
Henrique Ferrolho <henrique.ferrolho@gmail.com>
2021-01-10 20:24:40 +0000

    [2016] Renames folders and adds Julia files

---
+++ 2016/src/AdventOfCode2016.jl
@@ -0,0 +1,11 @@
+module AdventOfCode2016
+
+using BenchmarkTools
+using Combinatorics
+using LinearAlgebra
+
+greet() = println("Hello, Advent of Code 2016!")
+
+include("../04/puzzle.jl")
+
+end # module
